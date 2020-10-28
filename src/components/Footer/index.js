9d393ec05f3e4ace890ea38b4c93889a453a9910
Darkisda <luandesouzanascimento2011@gmail.com>
2020-10-28 11:31:13 -0300

    footer needs refactor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,20 @@ import './style.css';
 export default function Footer() {
   return (
     <Container fluid id="footer">
-      <h1>AUISHDAISUDH</h1>
+      <div className="footer-col">
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+      </div>
+      <div className="footer-col">
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+        <p>Exemplo</p>
+      </div>
     </Container>
   );
 }
