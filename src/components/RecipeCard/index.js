a53fdcdf0aff88dc46ffb09a2da05884ae9b25b7
Darkisda <luandesouzanascimento2011@gmail.com>
2020-11-02 11:32:47 -0300

    recipes page in comming

---
+++ src/components/RecipeCard/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './style.css';
+
+export default function RecipeCard() {
+  return (
+    <div className="recipe-card">
+      <div className="gradient-opacity" />
+      <img
+        src="https://i0.statig.com.br/bancodeimagens/cs/1t/wm/cs1twm81owp8iug586w3t6pjl.jpg"
+        alt="imagem da receita"
+        className="recipe-background"
+      />
+      <div className="recipe-content-description">
+        <h3 className="recipe-name">Nome da receita</h3>
+        <p className="recipe-description">
+          Descrição rápida a respeito da receita
+        </p>
+      </div>
+      <a href="/" className="recipe-link">
+        Veja a receira completa!
+      </a>
+    </div>
+  );
+}
--- src/pages/RecipesPage/index.js
+++ src/pages/RecipesPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import UserHeader from '../../components/UserHeader';
+import RecipeCard from '../../components/RecipeCard';
 
 import './style.css';
 
@@ -8,6 +9,34 @@ export default function RecipesPage() {
   return (
     <Container className="container-recipes">
       <UserHeader />
+      <Row>
+        <h1>
+          Algumas receitas que <br /> possam te interessar.
+        </h1>
+      </Row>
+      <Row>
+        <a href="/">Escreva agora mesmo uma nova receita!</a>
+      </Row>
+      <div className="recipes-content">
+        <Row>
+          <RecipeCard />
+        </Row>
+        <Row>
+          <RecipeCard />
+        </Row>
+        <Row>
+          <RecipeCard />
+        </Row>
+        <Row>
+          <RecipeCard />
+        </Row>
+        <Row>
+          <RecipeCard />
+        </Row>
+        <Row>
+          <RecipeCard />
+        </Row>
+      </div>
     </Container>
   );
 }
