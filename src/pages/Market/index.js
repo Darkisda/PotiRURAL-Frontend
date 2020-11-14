275a3b9865d70f1cdac7d53e2476ad3f51d30912
Darkisda <luandesouzanascimento2011@gmail.com>
2020-11-14 17:41:05 -0300

    pagination ok in market

--- src/pages/Market/index.js
+++ src/pages/Market/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Pagination } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import api from '../../server/api';
 import UserHeader from '../../components/UserHeader';
@@ -9,16 +9,25 @@ export default function Market() {
   const [barracas, setBarracas] = useState([]);
   const [isLoaded, setLoaded] = useState(false);
 
-  async function fetchBarracas() {
-    const response = await api.get('market?page=2&limit=9');
+  const [page, setPage] = useState(1);
+  const [pageInfo, setPageInfo] = useState({ totalCount: 0, limit: 9 });
+  const [totalPages, setTotalPages] = useState(0);
 
+  async function fetchBarracas() {
+    const response = await api.get(`market?page=${page}&limit=9`);
+
+    setPageInfo({
+      totalCount: response.data.totalCount,
+      limit: response.data.limit,
+    });
     setBarracas(response.data.data);
+    setTotalPages(Math.trunc(pageInfo.totalCount / pageInfo.limit) + 1);
     setLoaded(true);
   }
 
   useEffect(() => {
     fetchBarracas();
-  }, []);
+  }, [page]);
 
   return (
     <Container className="custom-container">
@@ -33,18 +42,32 @@ export default function Market() {
       </Row>
       <Row className="custom-row">
         {isLoaded ? (
-          barracas.map((barraca) => (
-            <Col
-              key={barraca.id}
-              lg={4}
-              sm={12}
-              xs={12}
-              md={4}
-              className="custom-col"
-            >
-              <BarracaCard barraca={barraca} />
-            </Col>
-          ))
+          <>
+            <Row className="custom-row">
+              <Pagination>
+                <Pagination.First onClick={() => setPage(1)} />
+                <Pagination.Prev
+                  onClick={() => setPage(page <= 1 ? 1 : page - 1)}
+                />
+                <Pagination.Next
+                  onClick={() => setPage(page >= totalPages ? page : page + 1)}
+                />
+                <Pagination.Last onClick={() => setPage(totalPages)} />
+              </Pagination>
+            </Row>
+            {barracas.map((barraca) => (
+              <Col
+                key={barraca.id}
+                lg={4}
+                sm={12}
+                xs={12}
+                md={4}
+                className="custom-col"
+              >
+                <BarracaCard barraca={barraca} />
+              </Col>
+            ))}
+          </>
         ) : (
           <h1>Loading...</h1>
         )}
