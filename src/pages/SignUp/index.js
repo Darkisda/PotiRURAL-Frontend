5a3c07277c2b678fce21a9b19a5a19b1d7c34617
Darkisda <luandesouzanascimento2011@gmail.com>
2020-11-22 11:26:37 -0300

    registration in comming

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Container, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../server/api';
 
 import './style.css';
 
 export default function SignUp() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [occupation, setOccupation] = useState('');
+  const [whatsapp, setWhatsapp] = useState(0);
+
+  const history = useHistory();
+
+  const occupations = [
+    {
+      label: 'Produtor',
+      value: 'PRODUTOR',
+    },
+    {
+      label: 'Professor',
+      value: 'PROFESSOR',
+    },
+    {
+      label: 'Aluno',
+      value: 'ALUNO',
+    },
+  ];
+
+  function handleSucess() {
+    alert('Cadastro realizado com sucesso!');
+    history.push('/signin');
+  }
+
+  async function handleSignUp() {
+    // console.log(firstName, lastName, whatsapp, occupation, email, password);
+
+    const response = await api.post('auth/signup', {
+      firstName,
+      lastName,
+      whatsapp,
+      occupation,
+      email,
+      password,
+    });
+
+    return response;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleSignUp().then((response) => {
+      if (response.status === 201) {
+        handleSucess();
+      } else if (response.status === 400) {
+        console.log(response.data);
+      }
+    });
+  }
+
   return (
     <Container className="signup-container">
       <div className="form-header">
@@ -15,52 +71,94 @@ export default function SignUp() {
           <h1 id="potirural">potiRURAL</h1>
         </Link>
       </div>
-      <Form className="form-signup">
+      <Form className="form-signup" onSubmit={handleSubmit}>
         <Form.Row className="custom-row-form">
           <Form.Group as={Col} controlId="signup-firstname">
             <Form.Label className="custom-label"> Primeiro Nome: </Form.Label>
-            <Form.Control type="text" required />
+            <Form.Control
+              type="text"
+              required
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
           </Form.Group>
           <Form.Group as={Col} controlId="signup-lastname">
             <Form.Label className="custom-label"> Segundo Nome: </Form.Label>
-            <Form.Control type="text" required />
+            <Form.Control
+              type="text"
+              required
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
           </Form.Group>
         </Form.Row>
         <Form.Row className="custom-row-form">
           <Form.Group as={Col} controlId="signup-email">
             <Form.Label className="custom-label"> Email: </Form.Label>
-            <Form.Control type="email" required />
+            <Form.Control
+              type="email"
+              required
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </Form.Group>
         </Form.Row>
         <Form.Row className="custom-row-form">
           <Form.Group as={Col} controlId="signup-password">
             <Form.Label className="custom-label"> Senha:</Form.Label>
-            <Form.Control type="password" required />
-          </Form.Group>
-          <Form.Group as={Col} controlId="signup-checkpassword">
-            <Form.Label className="custom-label">
-              {' '}
-              Confirme sua senha:
-            </Form.Label>
-            <Form.Control type="password" />
+            <Form.Control
+              type="password"
+              required
+              pattern="((?=.*[A-Z])(?=.*[a-z]).*$)"
+              title="A senha deve conter: pelo menos 1 letra Maiúscula, 1  letra Minúscula e 1 Número"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <p>
+              A senha deve conter: 1 Letra Maiúscula, 1 Letra Minúscula e 1
+              Número
+            </p>
           </Form.Group>
         </Form.Row>
         <Form.Row className="custom-row-form">
           <Form.Group as={Col} controlId="whatsapp">
             <Form.Label className="custom-label"> Whatsapp:</Form.Label>
-            <Form.Control type="text" required />
+            <Form.Control
+              type="number"
+              required
+              onChange={(e) => {
+                setWhatsapp(e.target.value);
+              }}
+            />
           </Form.Group>
           <Form.Group as={Col} controlId="ocuppation">
             <Form.Label className="custom-label"> Ocupação:</Form.Label>
-            <Form.Control as="select">
-              <option>Produtor</option>
-              <option>Professor</option>
-              <option>Estudante</option>
+            <Form.Control
+              as="select"
+              defaultValue="DEFAULT"
+              onChange={(e) => {
+                setOccupation(e.target.value);
+              }}
+            >
+              <option disabled hidden value="DEFAULT">
+                Selecione uma ocupação
+              </option>
+              {occupations.map((occu) => (
+                <option key={occu.value} value={occu.value}>
+                  {occu.label}
+                </option>
+              ))}
             </Form.Control>
           </Form.Group>
         </Form.Row>
         <Form.Row className="custom-row-form-button">
-          <Button className="signup-button">Cadastrar</Button>
+          <Button className="signup-button" type="submit">
+            Cadastrar
+          </Button>
         </Form.Row>
       </Form>
     </Container>
