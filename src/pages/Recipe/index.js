e7013510dd9fcbf8a771b82fd67b43ca41824765
Darkisda <luandesouzanascimento2011@gmail.com>
2020-11-28 22:29:20 -0300

    paragraphs in recipe

--- src/pages/Recipe/index.js
+++ src/pages/Recipe/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import api from '../../server/api';
@@ -34,20 +35,24 @@ export default function Recipe(props) {
           <hr />
           <Row className="custom-row">
             <h3 className="recipes-subtitle-page">Ingredientes:</h3>
-            <p>
+            <ul>
               {recipe.ingredients !== undefined
-                ? recipe.ingredients.split(/\n|\n\n/)
+                ? recipe.ingredients
+                    .split(/\n|\n\n/)
+                    .map((paragraph, idx) => <li key={idx}>{paragraph}</li>)
                 : ''}
-            </p>
+            </ul>
           </Row>
           <Row className="custom-row">
             <h3 className="recipes-subtitle-page">Modo de Preparo:</h3>
-            <p>
-              {recipe.preparation !== undefined
-                ? recipe.preparation.split(/\n|\n\n/)
-                : ''}
-            </p>
           </Row>
+          {recipe.preparation !== undefined
+            ? recipe.preparation.split(/\n|\n\n/).map((paragraph, idx) => (
+                <Row className="custom-row" key={idx}>
+                  {paragraph}
+                </Row>
+              ))
+            : ''}
         </>
       ) : (
         <>
